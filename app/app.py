6d5aa47437c952015f92ed82a7dba63a7b446fc8
borktrain <bourque125@yahoo.com>
2019-03-26 13:57:55 +0000

    added geT_plc_stat to app to allow for poling

--- app/app.py
+++ app/app.py
@@ -11,12 +11,12 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import url_for
+from reg_reader import *
 from reg_sender import *
 from functools import wraps
 
 app = Flask(__name__, static_url_path='')
 app.secret_key = 'secret_key_'
-stat = {}
 
 def login_required(f):
     @wraps(f)
@@ -43,73 +43,54 @@ def login_route():
 @login_required
 def hmi_route():
 	if request.method == "POST":
+		get_plc_stat = get_plc_stat()
 		if request.form.get('fuel_rate'):
 			print(request.form.get('fuel_rate'))
 			if request.form.get('fuel_rate') == '0':
-				stat['fuel_stat'] = "OFF"
 				fuel_rate_0()
 			elif request.form.get('fuel_rate') == '1':
-				stat['fuel_stat'] = "LOW"
 				fuel_rate_1()
 			elif request.form.get('fuel_rate') == '2':
-				stat['fuel_stat'] = "NORMAL"
 				fuel_rate_2()
 			else:
-				stat['fuel_stat'] = "MAX"
 				fuel_rate_3()
 		if request.form.get('waterpump_io'):
 			print(request.form.get('waterpump_io'))
 			if request.form.get('waterpump_io') == '1':
-				stat['waterpump_stat'] = "ON"
 				waterpump_on()
 			else:
-				stat['waterpump_stat'] = "OFF"
 				waterpump_off()
 		if request.form.get('boiler_io'):
 			print(request.form.get('boiler_io'))
 			if request.form.get('boiler_io') == '1':
-				stat['boiler_stat'] = "ON"
 				boiler_on()
 			else:
-				stat['boiler_stat'] = "OFF"
 				boiler_off()
 		if request.form.get('turbine_io'):
 			print(request.form.get('turbine_io'))
 			if request.form.get('turbine_io') == '1':
-				stat['turbine_stat'] = "ON"
 				turbine_on()
 			else:
-				stat['turbine_stat'] = "OFF"
 				turbine_off()
 		if request.form.get('generator_io'):
 			print(request.form.get('generator_io'))
 			if request.form.get('generator_io') == '1':
-				stat['generator_stat'] = "ON"
 				generator_on()
 			else:
-				stat['generator_stat'] = "OFF"
 				generator_off()
 		if request.form.get('pylon_io'):
 			print(request.form.get('pylon_io'))
 			if request.form.get('pylon_io') == '1':
-				stat['pylon_stat'] = "ON"
 				pylon_on()
 			else:
-				stat['pylon_stat'] = "OFF"
 				pylon_off()
 		if request.form.get('zero_out'):
 			print (request.form.get('zero_out'))
 			if request.form.get('zero_out') == '0':
-				stat['fuel_stat'] = "OFF"
-				stat['waterpump_stat'] = "OFF"
-				stat['boiler_stat'] = "OFF"
-				stat['turbine_stat'] = "OFF"
-				stat['generator_stat'] = "OFF"
-				stat['pylon_stat'] = "OFF"
 				emergency_shutoff()
 		else:
 			pass
-	return render_template('hmi.html',stat=stat)
+	return render_template('hmi.html',get_plc_stat=get_plc_stat)
 
 @app.route('/status',methods=['GET','POST'])
 @login_required
---
+++ app/reg_reader.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import time
+
+PLCS = {'FUEL':'10.10.1.120','WATERPUMP':'10.10.1.121','BOILER':'10.10.1.122','TURBINE':'10.10.1.123','GENERATOR':'10.10.1.124','PYLON':'10.10.1.125'}
+
+def send_to_plc(reg_loc,reg_value,ip):
+	from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+	modbusClient = ModbusClient(ip, port=5020)
+	modbusClient.write_register(reg_loc,reg_value)
+	return True
+
+def read_from_plc(reg_start,reg_stop,ip):
+	from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+	modbusClient = ModbusClient(ip, port=5020)
+	res = modbusClient.read_holding_registers(reg_start,reg_stop).registers
+	return res
+
+def get_plc_stat():
+	res = {}
+	# res = {'FUEL':'off'|'low'|'norm'|'max','WATERPUMP':'off'|'on'}
+	for plc_name in PLCS:
+		plc_ip = PLCS.get(plc_name)
+		plc_vals = read_from_plc(1,16,plc_ip)
+		if plc_name is 'FUEL':
+			res[plc_name] = get_fuel_stat(plc_vals)
+		elif plc_name is 'WATERPUMP':
+			res[plc_name] = get_water_stat(plc_vals)
+		elif plc_name is 'BOILER':
+			res[plc_name] = get_boiler_stat(plc_vals)
+		elif plc_name is 'TURBINE':
+			res[plc_name] = get_turbine_stat(plc_vals)
+		elif plc_name is 'GENERATOR':
+			res[plc_name] = get_generator_stat(plc_vals)
+		elif plc_name is 'PYLON':
+			res[plc_name] = get_pylon_stat(plc_vals)
+	return res
+
+def get_fuel_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 64:
+		rate = 'max'
+	elif plc_sum == 32:
+		rate = 'norm'
+	elif plc_sum == 16:
+		rate = 'low'
+	else:
+		rate = 'off'
+	return rate
+
+def get_water_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 16:
+		rate = 'on'
+	else:
+		rate = 'off'
+	return rate
+
+def get_boiler_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 16:
+		rate = 'open'
+	else:
+		rate = 'closed'
+	return rate
+
+def get_turbine_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 16:
+		rate = 'on'
+	else:
+		rate = 'off'
+	return rate
+
+def get_generator_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 16:
+		rate = 'on'
+	else:
+		rate = 'off'
+	return rate
+
+def get_pylon_stat(plc_vals):
+	plc_sum = sum(plc_vals)
+	if plc_sum == 16:
+		rate = 'on'
+	else:
+		rate = 'off'
+	return rate
+
+
+
+
+
+'''
+def prod_on():
+	x = get_plc_stat()
+	y = x.get('WATERPUMP')
+	z = x.get('FUEL')
+	if y.__contains__('on') and z != 'fuel_off':
+		send_to_plc(10,1,PLCS.get('WATERPUMP'))
+		send_to_plc(10,1,PLCS.get('BOILER'))
+		send_to_plc(10,1,PLCS.get('TURBINE'))
+		send_to_plc(10,1,PLCS.get('GENERATOR'))
+		send_to_plc(10,1,PLCS.get('PYLON'))
+	else:
+		pass
+	return True 
+
+def plc_rate():
+	x = get_plc_stat()
+	y = x.get('WATERPUMP')
+	z = x.get('FUEL')
+	if y.__contains__('on') and z == 'fuel_low':
+		send_to_plc(11,1,PLCS.get('BOILER'))
+		send_to_plc(11,1,PLCS.get('TURBINE'))
+		send_to_plc(11,1,PLCS.get('GENERATOR'))
+		send_to_plc(11,1,PLCS.get('PYLON'))
+	elif y.__contains__('on') and z == 'fuel_norm':
+		send_to_plc(11,2,PLCS.get('BOILER'))
+		send_to_plc(11,2,PLCS.get('TURBINE'))
+		send_to_plc(11,2,PLCS.get('GENERATOR'))
+		send_to_plc(11,2,PLCS.get('PYLON'))
+	elif y.__contains__('on') and z == 'fuel_max':
+		send_to_plc(11,3,PLCS.get('BOILER'))
+		send_to_plc(11,3,PLCS.get('TURBINE'))
+		send_to_plc(11,3,PLCS.get('GENERATOR'))
+		send_to_plc(11,3,PLCS.get('PYLON'))
+	else:
+		pass
+	return True
+'''
